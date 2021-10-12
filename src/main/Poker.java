02829857c9bdd7cb01422629c32fad4ea7859305
Tina Hwang <tina_thedreamer@outlook.com>
2021-10-11 20:52:47 -0400

    modify Poker class

--- src/main/Poker.java
+++ src/main/Poker.java
@@ -1,47 +1,51 @@
 package main;
 
-import static main.Suits.*;
-
-public class Poker extends CardSet {
+final class Poker extends CardSet {
     boolean joker;
-    Suits[] suits = {HEARTS, DIAMONDS, CLUBS, SPADES};
-    private static Poker instance = new Poker();
+    private static Poker INSTANCE = null;
+
+    public static Poker getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Poker();
+        }
+        return INSTANCE;
+    }
 
     private Poker () {
         int n = 10;
 
-        for (int i = 0; i < suits.length; i++) {
+        for (int i = 0; i < Suits.values().length; i++) {
             for (int j = 1; j <= n; j++) {
                 if (j == 1) {
-                    this.addCard(new Card(suits[i], "A", j));
+                    this.addCard(new Card(Suits.values()[i], "A", j));
                     continue;
                 }
-                this.addCard(new Card(suits[i], j + "", j));
+                this.addCard(new Card(Suits.values()[i], String.valueOf(j), j));
             }
-            this.addCard(new Card(suits[i], "J", 10));
-            this.addCard(new Card(suits[i], "Q", 10));
-            this.addCard(new Card(suits[i], "K", 10));
+            this.addCard(new Card(Suits.values()[i], "J", 10));
+            this.addCard(new Card(Suits.values()[i], "Q", 10));
+            this.addCard(new Card(Suits.values()[i], "K", 10));
         }
 
     }
 
 
-    public Poker (int jack, int queen, int king) {
+    private Poker (int jack, int queen, int king) {
         /* set J, Q, K with specified value */
 
         int n = 10;
 
-        for (int i = 0; i < suits.length; i++) {
+        for (int i = 0; i < Suits.values().length; i++) {
             for (int j = 1; j <= n; j++) {
                 if (j == 1) {
-                    this.addCard(new Card(suits[i], "Ace", j));
+                    this.addCard(new Card(Suits.values()[i], "Ace", j));
                     continue;
                 }
-                this.addCard(new Card(suits[i], j + "", j));
+                this.addCard(new Card(Suits.values()[i], String.valueOf(j), j));
             }
-            this.addCard(new Card(suits[i], "Jack", jack));
-            this.addCard(new Card(suits[i], "Queen", queen));
-            this.addCard(new Card(suits[i], "King", king));
+            this.addCard(new Card(Suits.values()[i], "Jack", jack));
+            this.addCard(new Card(Suits.values()[i], "Queen", queen));
+            this.addCard(new Card(Suits.values()[i], "King", king));
         }
     }
 }
