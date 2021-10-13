fd9824ec5d0f981a92c3ad9745032cd04e134698
Tina Hwang <tina_thedreamer@outlook.com>
2021-10-13 09:58:22 -0400

    add "bust" status"

--- src/main/AbstractPlayer.java
+++ src/main/AbstractPlayer.java
@@ -6,6 +6,7 @@ abstract class AbstractPlayer {
     private Money bet;
     private CardSet cardsInHand;
     private boolean stand;
+    private boolean bust;
 
     public Money getBalance() {
         return balance;
@@ -23,7 +24,10 @@ abstract class AbstractPlayer {
         this.bet = new Money(0);
         this.cardsInHand = new CardSet();
         this.stand = false;
+        this.bust = false;
     }
+
+
 
     public CardSet getCardsInHand() {
         return cardsInHand;
@@ -45,12 +49,22 @@ abstract class AbstractPlayer {
         this.stand = stand;
     }
 
+    public boolean isBust() {
+        return bust;
+    }
+
+    public void setBust(boolean bust) {
+        this.bust = bust;
+    }
+
     private void init() {
         /* each round init player's cardsInHand and action status */
         if (this.cardsInHand.getSize() == 0) {
             return;
         }
         clearCardsHold();
+        setBust(false);
+        setStand(false);
     }
 
     public void deal(Card[] cards) {
