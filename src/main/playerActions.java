8135ab24e8d5e6564f31dfa584cecbb8e086acb9
Tina Hwang <tina_thedreamer@outlook.com>
2021-10-12 23:17:59 -0400

    modify enums: add id

--- src/main/playerActions.java
+++ src/main/playerActions.java
@@ -1,18 +1,22 @@
 package main;
 
 enum playerActions {
-    HIT("hi"),
-    STAND("st"),
-    SPLIT("sp"),
-    DOUBLEUP("db"),
-    DEAL("dl");
+    HIT("hit", 1),
+    STAND("stand", 2),
+    SPLIT("split", 3),
+    DOUBLEUP("double up", 4),
+    DEAL("deal", 0);
 
     private final String symbol;
-    playerActions(String symbol) {
+    private final int id;
+    playerActions(String symbol, int id) {
         this.symbol = symbol;
+        this.id = id;
     }
 
     public String getSymbol() {
         return symbol;
     }
+
+    public int getId() {return id;}
 }
